d5aeb0892b9cdedd77f44b6daa568a28c2444069
Andremachado82 <andrels.machado82@gmail.com>
2021-07-14 14:03:01 -0300

    Inserido swagger para gerar a doc. da api

--- src/main/java/com/dio/live/controllers/JornadaTrabalhoController.java
+++ src/main/java/com/dio/live/controllers/JornadaTrabalhoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,8 +22,13 @@ import com.dio.live.models.JornadaTrabalho;
 import com.dio.live.repositories.JornadaTrabalhoRepository;
 import com.dio.live.services.JornadaTrabalhoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/jornada")
+@Api(value="API REST Jornada de Trabalho")
+@CrossOrigin(origins = "*")
 public class JornadaTrabalhoController {
 	
 	@Autowired
@@ -39,6 +45,7 @@ public class JornadaTrabalhoController {
 	}
 	
 	@GetMapping("/listar")
+	@ApiOperation(value="Retorna uma lista de jornadas de trabalho")
 	public List<JornadaTrabalho> buscarLista() {
 		List<JornadaTrabalho> jornada = jornadaTrabalhoService.buscarLista();
 		
